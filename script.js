166518a7cf8dbad1af34efffe2f45a2621f4fe07
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-11-13 17:40:33 -0600

    updated assignment with corrections

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ $(document).ready(readyNow)
     function readyNow(){
     console.log('JQ');
     $('#generateButton').on('click', generateButtonPush);
-    $(this).on('click', "#swapButton", swapButtonColorChanger);
-    $(this).on('click', '#deleteButton', deleteFunction);
+    $(this).on('click', ".swapButton", swapButtonColorChanger);
+    $(this).on('click', '.deleteButton', deleteFunction);
     
     }
 
@@ -14,28 +14,22 @@ $(document).ready(readyNow)
 function generateButtonPush(){
     console.log('in generate button push function');
 
-    // $('body').append('<div id = "firstDiv"><p id = "generateButtonClicks"></p><p><button id = "swapButton">Swap</button></p><p><button id = "deleteButton">Delete</button></p></div>');
-    
-    $('body').append('<div id = "firstDiv"><p><button id = "swapButton">Swap</button><button id = "deleteButton">Delete</button></p></div>');
-    clickCounter++;
-    // document.getElementById("generateButtonClicks").innerHTML = clickCounter;
-    $('body').append(clickCounter);
-    // clickCounter++
-    // $("p").html() = clickCounter;
+    clickCounter ++;
+    $('body').append('<div>' + 
+    '<p>'+ clickCounter + '</p>' +
+    '<button class = "swapButton">Swap</button>'+
+    '<button class = "deleteButton">Delete</button></div>');
 }
 
 function swapButtonColorChanger(  ){
     console.log('in swapbuttoncolor changer function');
-    
-    $(this).parent().css('background-color','yellow');
-    // $().parent().css('background-color','yellow');
-    // $('#generateButtonClicks').parent().css('background-color','yellow');
-    
-}
-  
+        $(this).parent().toggleClass('yellow');
+};
+
+
+
 function deleteFunction(){
     console.log('in delete function');
-    $(this).parent().empty();
-    // $('#swapButton').parent().empty();
-  // $('body').remove(clickCounter);
+    $(this).parent().remove();
 }
+
